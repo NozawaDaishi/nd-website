470985f29a7c293c52855241cb5e11e4a20f7bc4
NozawaDaishi <nozawa171040@gmail.com>
2022-09-07 18:00:42 +0900

    heroに画像が必要なページのために引数を追加

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Hero from 'components/hero'
 
 export default function About() {
-  return <Hero title="Project ND" subtitle="Nozawa Daishiのアウトプット" />
+  return (
+    <Hero title="Project ND" subtitle="Nozawa Daishiのアウトプット" imageOn />
+  )
 }
