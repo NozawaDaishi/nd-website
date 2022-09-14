27a67fd9ecca226154b0815fd4204d5ccb16a09c
d-nozawa <d-nozawa@usen-next.jp>
2022-09-15 00:11:20 +0900

    containerコンポーネントで幅を揃える

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,16 @@
 import styles from 'styles/footer.module.css'
 import Logo from 'components/logo'
+import Container from 'components/container'
 
 export default function Footer() {
   return (
-    <footer>
-      <Logo />
+    <footer className={styles.wrapper}>
+      <Container>
+        <div className={styles.flexContainer}>
+          <Logo />
+          [ソーシャル]
+        </div>
+      </Container>
     </footer>
   )
 }
--- components/header.js
+++ components/header.js
@@ -1,12 +1,17 @@
 import styles from 'styles/header.module.css'
 import Logo from 'components/logo'
 import Nav from 'components/nav'
+import Container from 'components/container'
 
 export default function Header() {
   return (
     <header>
-      <Logo boxOn />
-      <Nav />
+      <Container large>
+        <div className={styles.flexContainer}>
+          <Logo boxOn />
+          <Nav />
+        </div>
+      </Container>
     </header>
   )
 }
