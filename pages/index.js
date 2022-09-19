e9701e94f67ecec8ec7521b288c1c704ecdfc31d
NozawaDaishi <nozawa171040@gmail.com>
2022-09-19 11:41:52 +0900

    Metaデータの追加

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function About() {
   return (
     <Container>
+      <Meta />
       <Hero title="Project ND" subtitle="Nozawa Daishiのアウトプット" imageOn />
     </Container>
   )
