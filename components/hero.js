43fd798b2a27d441f68e1568793e9458980576e4
NozawaDaishi <nozawa171040@gmail.com>
2022-09-22 14:38:59 +0900

    404ページの画像変更とheroでの表示に変更

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,9 @@
 import styles from 'styles/hero.module.css'
 import Image from 'next/image'
 import D from 'images/D.png'
+import notFound from 'images/black-hole.png'
 
-export default function Hero({ title, subtitle, imageOn = false }) {
+export default function Hero({ title, subtitle, imageOn = false, notFoundImageOn = false }) {
   return (
     <div className={styles.flexContainer}>
       <div className={styles.text}>
@@ -13,6 +14,19 @@ export default function Hero({ title, subtitle, imageOn = false }) {
         <figure className={styles.image}>
           <Image
             src={D}
+            alt=""
+            layout="responsive"
+            quality={100}
+            sizes="(min-width: 1152px) 576px, (min-width: 768px) 50vw, 100vw"
+            priority
+            placeholder="blur"
+          />
+        </figure>
+      )}
+      {notFoundImageOn && (
+        <figure className={styles.image}>
+          <Image
+            src={notFound}
             alt=""
             layout="responsive"
             quality={100}
--- pages/404.js
+++ pages/404.js
@@ -2,18 +2,16 @@ import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Image from 'next/image'
-import eyecatch from 'images/about.jpg'
+import notFound from 'images/black-hole.png'
+import styles from 'styles/404.module.css'
 
 export default function Custom404() {
   return (
     <Container>
       <Meta pageTitle="404 - Page not found" />
-
-      <Hero title="404" subtitle="ページが見つかりません" />
-
-      <figure>
-        <Image src={eyecatch} alt="" layout="responsive" sizes="(min-width: 1152px) 1152px, 100vw" priority placeholder="blur" />
-      </figure>
+      <div className={styles.flexContainer}>
+        <Hero title="404" subtitle="ページが見つかりません" notFoundImageOn />
+      </div>
     </Container>
   )
 }
